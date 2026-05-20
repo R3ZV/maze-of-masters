82f65f6f974fe541356b6290465b636b8971c2cd
Neacsu Mihnea <neomihnea@R.Mihnea>
2026-05-20 21:48:42 +0300

    feat: added socket to talk to patches

---
+++ music/interface.js
@@ -0,0 +1,37 @@
+import {WebPdWorkletNode, registerWebPdWorkletNode } from '@webpd/runtime'; 
+
+export class WebPdSocket {
+    constructor(path) {
+        this.node = null;
+        this.path = path;
+        this.ready = false;
+    }
+
+    async listen() {
+        if (this.ready) return;
+         
+        const ctx = new AudioContext();
+        await ctx.resume();
+       
+        await registerWebPdWorkletNode(ctx);
+
+        const response = await fetch(this.path);
+        const code = await response.text();
+
+        this.node = new WebPdWorkletNode(ctx);
+        this.node.port.postMessage({ type: 'code:JS', payload: { jsCode: code } });
+        this.node.connect(ctx.destination);
+
+        this.ready = true;
+        console.log("[INFO]: two nil to the arsenal");
+    }
+
+    send(receiver, value) {
+        if (this.ready) {
+            this.node.port.postMessage({
+                type: 'inletCaller',
+                payload: { nodeId: receiver, portletId: '0', message: [value] }
+            });
+        }
+    }
+}
